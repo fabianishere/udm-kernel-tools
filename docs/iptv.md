dd162a8087a92fbbc329cb4516596d86dca3d988
Fabian Mastenbroek <mail.fabianm@gmail.com>
2021-08-27 12:57:51 +0200

    Automatically determine default WAN interface
    This change updates the IPTV install script to automatically determine
    the default WAN interface on which the IPTV data will enter the router.
    It can now determine whether the machine is running on an UDMP or UDMB.

--- docs/iptv.md
+++ docs/iptv.md
@@ -133,7 +133,7 @@ specific setup. See below for a full list of options to configure the container.
 
 | Environmental Variable | Description | Default |
 | ------------------------|----------- |---------|
-| IPTV_WAN_INTERFACE      | Interface on which IPTV traffic enters the router | eth8 |
+| IPTV_WAN_INTERFACE      | Interface on which IPTV traffic enters the router | eth8 (on UDM Pro) or eth4 (on UDM) |
 | IPTV_WAN_RANGES         | IP ranges from which the IPTV traffic originates (separated by spaces) | 213.75.0.0/16 217.166.0.0/16 |
 | IPTV_WAN_VLAN           | ID of VLAN which carries IPTV traffic (use 0 if no VLAN is used) | 4 |
 | IPTV_WAN_VLAN_INTERFACE | Name of the VLAN interface to be created | iptv |
--- docs/iptv/install.sh
+++ docs/iptv/install.sh
@@ -14,8 +14,11 @@ then
     exit 1
 fi
 
-tee  /mnt/data/on_boot.d/15-iptv.sh <<'EOF' >/dev/null
-IPTV_WAN_INTERFACE="eth8"
+# Determine the default WAN port (eth8 on UDM Pro, eth4 on UDM Base)
+DEFAULT_WAN_PORT=$(grep -q "UDMPRO" /etc/board.info && echo "eth8" || echo "eth4")
+
+tee  /mnt/data/on_boot.d/15-iptv.sh <<EOF >/dev/null
+IPTV_WAN_INTERFACE="$DEFAULT_WAN_PORT"
 IPTV_WAN_RANGES="213.75.112.0/21 217.166.0.0/16"
 IPTV_WAN_VLAN="4"
 IPTV_WAN_DHCP_OPTIONS="-O staticroutes -V IPTV_RG"
@@ -25,14 +28,14 @@ IPTV_LAN_RANGES="192.168.0.0/16"
 if podman container exists iptv; then
   podman rm -f iptv
 fi
-podman run --network=host --privileged \
-    --name iptv -i -d --restart always \
-    -e IPTV_WAN_INTERFACE="$IPTV_WAN_INTERFACE" \
-    -e IPTV_WAN_RANGES="$IPTV_WAN_RANGES" \
-    -e IPTV_WAN_VLAN="$IPTV_WAN_VLAN" \
-    -e IPTV_WAN_DHCP_OPTIONS="$IPTV_WAN_DHCP_OPTIONS" \
-    -e IPTV_LAN_INTERFACES="$IPTV_LAN_INTERFACES" \
-    -e IPTV_LAN_RANGES="$IPTV_LAN_RANGES" \
+podman run --network=host --privileged \\
+    --name iptv -i -d --restart always \\
+    -e IPTV_WAN_INTERFACE="\$IPTV_WAN_INTERFACE" \\
+    -e IPTV_WAN_RANGES="\$IPTV_WAN_RANGES" \\
+    -e IPTV_WAN_VLAN="\$IPTV_WAN_VLAN" \\
+    -e IPTV_WAN_DHCP_OPTIONS="\$IPTV_WAN_DHCP_OPTIONS" \\
+    -e IPTV_LAN_INTERFACES="\$IPTV_LAN_INTERFACES" \\
+    -e IPTV_LAN_RANGES="\$IPTV_LAN_RANGES" \\
     fabianishere/udm-iptv
 EOF
 chmod +x /mnt/data/on_boot.d/15-iptv.sh
