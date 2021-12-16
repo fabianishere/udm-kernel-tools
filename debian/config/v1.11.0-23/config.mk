a50ce31a86e54f1efa7e183450c688c97b773085
Jason Hobbs <telnetdoogie@gmail.com>
2021-12-16 01:55:14 -0600

    pre-build changes for 1.11.0-23

---
+++ debian/config/v1.11.0-23/config.mk
@@ -0,0 +1,11 @@
+# -*- makefile -*-
+
+# Copyright (C) 2021 Fabian Mastenbroek.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+
+UDM_VERSION = 1.11.0-23
+UDM_COMMIT = 89130462d8a36d535c1d0ce50a7a153ad1587865
